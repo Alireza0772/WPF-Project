011805208f6e4bad66b2cb445c35bdf273942c42
Alireza Senobari <alirezasnbr@hotmail.com>
2020-03-27 01:23:07 +0430

    Renamings

--- WPF Project/MainWindow.xaml.cs
+++ WPF Project/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPF_Project_1
+namespace WPFProject
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
--- WPF Project/Views/ShelfView.xaml.cs
+++ WPF Project/Views/ShelfView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPF_Project_1.Views
+namespace WPFProject.Views
 {
     /// <summary>
     /// Interaction logic for ShelfUC.xaml
