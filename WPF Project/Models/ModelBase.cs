2d8dd04acdf334087582514bef0434f085a193fc
Alireza Senobari <alirezasnbr@hotmail.com>
2020-05-27 14:41:02 +0430

    Added UserControlStyle

--- WPF Project/Models/ModelBase.cs
+++ WPF Project/Models/ModelBase.cs
@@ -12,7 +12,7 @@ namespace WPFProject.Models
     public class ModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
